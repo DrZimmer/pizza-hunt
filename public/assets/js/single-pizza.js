5443505659f5b5f028497fcbade97f46645dd1d3
White Tiger <dayley.zimmermann@gmail.com>
2022-03-29 15:28:08 -0400

    lesson3 finished

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -109,26 +109,74 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
-}
 
-function handleNewReplySubmit(event) {
-  event.preventDefault();
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  };
 
-  if (!event.target.matches('.reply-form')) {
-    return false;
+  function handleNewReplySubmit(event) {
+    event.preventDefault();
+  
+    if (!event.target.matches('.reply-form')) {
+      return false;
+    }
+  
+    const commentId = event.target.getAttribute('data-commentid');
+  
+    const writtenBy = event.target.querySelector('[name=reply-name]').value;
+    const replyBody = event.target.querySelector('[name=reply]').value;
+  
+    if (!replyBody || !writtenBy) {
+      return false;
+    }
+  
+    const formData = { writtenBy, replyBody };
+  
+    fetch(`/api/comments/${pizzaId}/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+        response.json();
+      })
+      .then(commentResponse => {
+        console.log(commentResponse);
+        location.reload();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-
-  const commentId = event.target.getAttribute('data-commentid');
-
-  const writtenBy = event.target.querySelector('[name=reply-name]').value;
-  const replyBody = event.target.querySelector('[name=reply]').value;
-
-  if (!replyBody || !writtenBy) {
-    return false;
-  }
-
-  const formData = { writtenBy, replyBody };
-}
+  
+  $backBtn.addEventListener('click', function() {
+    window.history.back();
+  });
 
 $backBtn.addEventListener('click', function() {
   window.history.back();
@@ -136,3 +184,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
